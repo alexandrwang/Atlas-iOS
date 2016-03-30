5ad39a52e4bfa90e7cd66da89ff2b5c8fb9b5964
Alexandr Wang <avatar09@gmail.com>
2016-03-30 01:08:52 -0400

    fix woohoo

--- Code/Controllers/ATLBaseConversationViewController.m
+++ Code/Controllers/ATLBaseConversationViewController.m
@@ -223,7 +223,7 @@ static CGFloat const ATLMaxScrollDistanceFromBottom = 150;
     UIEdgeInsets insets = self.collectionView.contentInset;
     CGFloat keyboardHeight = MAX(self.keyboardHeight, CGRectGetHeight(self.messageInputToolbar.frame));
     
-    insets.bottom = keyboardHeight + self.typingIndicatorInset;
+    insets.bottom = keyboardHeight + self.typingIndicatorInset + 50;
     self.collectionView.scrollIndicatorInsets = insets;
     self.collectionView.contentInset = insets;
     self.typingIndicatorViewBottomConstraint.constant = -keyboardHeight;
